0897b712c8b45827cd429a0071c5cb12c85a9699
Nazita470 <nazarenosalvatierra0@gmail.com>
2023-10-05 19:13:22 -0300

    Diseño, html, css realizado.Falvata hs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
-const obj1 = {
-    nombre:  "Holaaa",
-    apellido: "jdafnhja"
-}
 
-console.log(obj1);
+
+const articulosCarrito = []
+
+const contenedor_productos = document.querySelector(".seccion_productos .contenedor");
+//console.log(contenedor_productos)
+
+contenedor_productos.addEventListener("click", agregarCarrito)
+
+function agregarCarrito(evt){
+
+    if(evt.target.classList.contains("botonAgregar")){
+        const producto = evt.target.parentElement.parentElement
+        let img = producto.querySelector("img").src
+        let nombre_p = producto.querySelector(".nombreProducto").textContent
+        let precio = producto.querySelector(".precioProducto").textContent
+        
+       const p = {
+            nombre: nombre_p,
+            img: img,
+            precio: precio
+        }
+
+        console.log(p)
+    }
+}
